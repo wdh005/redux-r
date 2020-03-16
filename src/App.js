b824e85c75a9f4e8ed89a4ac8b138fc721e34463
Jonghyun <wdh008@naver.com>
2020-03-16 15:40:15 +0900

    frist

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import AddNumberRoot from "./components/AddNumberRoot"
+import DisplayNumberRoot from "./components/DisplayNumberRoot"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {number:0};
+  render() {
+    return (
+      <div className="App">
+        <h1>Root</h1>
+        <AddNumberRoot onClick={function(size){
+          this.setState({number:this.state.number + size});
+        }.bind(this)}></AddNumberRoot>
+        <DisplayNumberRoot number={this.state.number}></DisplayNumberRoot>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
